d3c2a4497aa8274386dba6c7820fa9667816406a
Robert Guderian <rob.guderian@gmail.com>
2017-12-06 10:17:15 -0600

    Updates to office hours, course data

--- src/components/ca/umanitoba/comp1020.js
+++ src/components/ca/umanitoba/comp1020.js
@@ -7,7 +7,9 @@ class Comp1020 extends Component {
         return(
             <div>
                 <umanImage />
-            <p>Teaching COMP 1020, M/W/F 2:30 pm - 3:20pm. Attendance is optional, but highly suggested.</p>
+            <p>Teaching COMP 1020, M/W/F 2:30 pm - 3:20pm. Attendance is optional,
+                but 10% of your grade is based on in-class testing done via
+                iClicker. These pop-quizes will be in most classes.</p>
             <p>Class-specific examples and details will be posted here.</p>
             </div>
         );
